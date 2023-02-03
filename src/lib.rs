98cb3e82bf8c3afefbe98cac0f6769e67de24de0
mteam88 <matthew8edelen@gmail.com>
2023-02-03 19:23:23 +0000

    add functions to interact with elem

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,28 @@
 use wasm_bindgen::prelude::*;
 use web_sys::{Document, HtmlElement};
 
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+fn document() -> web_sys::Document {
+    window()
+        .document()
+        .expect("should have a document on window")
+}
+
+fn body() -> web_sys::HtmlElement {
+    document().body().expect("document should have a body")
+}
+
 #[wasm_bindgen(start)]
 fn start() -> Result<(), JsValue> {
-    let window = web_sys::window().expect("should have a window in this context");
-    let document = window.document().expect("window should have a document");
 
     fn setup_clicker(document: &Document) {
         let num_clicks = document
@@ -13,7 +31,7 @@ fn start() -> Result<(), JsValue> {
         let a = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
             num_clicks.set_inner_html(&(event.client_x().to_string().to_owned() + ", " + &event.client_y().to_string()));
         });
-        document.body().expect("body doesn't exist")
+        body()
             .dyn_ref::<HtmlElement>()
             .expect("body be an `HtmlElement`")
             .add_event_listener_with_callback("mousemove", a.as_ref().unchecked_ref()).expect("set event failed");
@@ -21,6 +39,10 @@ fn start() -> Result<(), JsValue> {
         // See comments in `setup_clock` above for why we use `a.forget()`.
         a.forget();
     }
-    setup_clicker(&document);
+
+
+
+
+    setup_clicker(&document());
     Ok(())
 }
